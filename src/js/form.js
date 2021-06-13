a1f61c75046a77ba3b1b572120c1614c32e6460a
salenctawer <salenctawer@gmail.com>
2021-06-14 02:17:32 +0500

    form was added

---
+++ src/js/form.js
@@ -0,0 +1,5 @@
+let editBtn = document.querySelector(".profile-info__button");
+let form = document.querySelector(".form");
+editBtn.addEventListener("click", function(){
+    form.style.display = "flex";
+});
