6eef9335de97f594a1e041ad39ef1c1a0124127a
salenctawer <salenctawer@gmail.com>
2021-06-14 16:34:49 +0500

    fixed form

--- src/js/form.js
+++ src/js/form.js
@@ -1,23 +1,27 @@
 const editBtn = document.querySelector('.profile-info__button');
 const form = document.querySelector('.form');
-const body = document.querySelector('.body');
 const closeBtn = document.querySelector('.close');
-const saveBtn = document.querySelector('.form__button');
-const formName = document.querySelector('.form-name');
-const formDescription = document.querySelector('.form-description');
+const saveBtn = form.querySelector('.form__button');
+const formName = form.querySelector('.form-name');
+const formDescription = form.querySelector('.form-description');
 const profileName = document.querySelector('.profile-info__name');
 const profileDescription = document.querySelector('.profile-info__description');
+
 editBtn.addEventListener('click', function(){
-    // body.style.background = "#00000080";
     form.style.display = 'flex';
     closeBtn.style.display = 'flex';
+    formName.value=profileName.innerHTML;
+    formDescription.value=profileDescription.innerHTML;
 });
 closeBtn.addEventListener('click', function(){
     form.style.display = 'none';
     closeBtn.style.display = 'none';
 });
-saveBtn.addEventListener('click', function(e){
+form.addEventListener('submit', function(e){
     e.preventDefault();
-    profileName.innerHTML=formName.value
-    profileDescription.innerHTML=formDescription.value
+    profileName.innerHTML=formName.value;
+    profileDescription.innerHTML=formDescription.value;
+    form.style.display = 'none';
+    closeBtn.style.display = 'none';
 });
+
