7d69c905ab84903b0e207c3ce8bf427a69945d04
salenctawer <salenctawer@gmail.com>
2021-08-20 18:23:03 +0500

    inactive button was added

--- src/js/validate.js
+++ src/js/validate.js
@@ -20,7 +20,11 @@ const hideInputError = (formElement, inputElement) => {
     errorElement.classList.remove('form__input-error_active');
     errorElement.textContent = '';
 };
-
+const hasInvalidInput = (inputList) => {
+    return inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    });
+};
 const checkInputValidity = (formElement, inputElement) => {
     if (!inputElement.validity.valid) {
         showInputError(formElement, inputElement, inputElement.validationMessage);
@@ -28,12 +32,21 @@ const checkInputValidity = (formElement, inputElement) => {
         hideInputError(formElement, inputElement);
     }
 };
-
+const toggleButtonState = (inputList, buttonElement) => {
+    if (hasInvalidInput(inputList)) {
+      buttonElement.classList.add('form__button_inactive');
+    } else {
+      buttonElement.classList.remove('form__button_inactive');
+    }
+  };
 const setEventListeners = (formElement) => {
     const inputList = Array.from(formElement.querySelectorAll('.form__input'));
+    const buttonElement = formElement.querySelector('.form__button');
+    toggleButtonState(inputList, buttonElement);
     inputList.forEach((inputElement) => {
         inputElement.addEventListener('input', function () {
             checkInputValidity(formElement, inputElement);
+            toggleButtonState(inputList, buttonElement);
         });
     });
 };
